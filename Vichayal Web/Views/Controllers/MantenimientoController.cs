7407f6db412b1bbce812678f63b7f62167b0a9f1
AlvaroSLL <108374848+AlvaroSLL@users.noreply.github.com>
2022-12-13 22:04:03 -0500

    Add files via upload

---
+++ Vichayal Web/Views/Controllers/MantenimientoController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Capa_Modelo;
+using Capa_Negocio;
+
+namespace Vichayal_Web.Controllers
+{
+    public class MantenimientoController : Controller
+    {
+        // GET: Mantenimiento
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Estudiantes()
+        {
+
+            return View();
+        }
+
+        public JsonResult ListarEstudiante()
+        {
+            List<Estudiante> eLista = new List<Estudiante>();
+            eLista = new CN_Estudiante().CN_ListarEstudiante();
+            return Json(new {data = eLista},JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
