386ec2d28b69bdd81d20729576b6c2370a13d92b
AlvaroSLL <108374848+AlvaroSLL@users.noreply.github.com>
2022-12-13 21:24:19 -0500

    Add files via upload

---
+++ Capa Modelo/Estudiante.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capa_Modelo
+{
+    public class Estudiante
+    {
+        public string ESTUDIANTE_ID { get; set; }
+        public string EST_DNI { get; set; }
+        public string EST_NOMBRES { get; set; }
+        public string EST_APELLIDOS { get; set; }
+        public string EST_FECHA { get; set; }
+        public string EST_DISTRITO { get; set; }
+        public string EST_DIRECCION { get; set; }
+        public string APODERADO_ID { get; set; }
+        public string EST_USUARIO { get; set; }
+    }
+}
---
+++ Capa Negocio/CN_Estudiante.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Capa_Datos;
+using Capa_Modelo;
+
+namespace Capa_Negocio
+{
+    public class CN_Estudiante
+    {
+        private CD_Estudiante objCapaDato = new CD_Estudiante();
+
+        public List<Estudiante> CN_ListarEstudiante()
+        {
+            return objCapaDato.CD_ListarEstudiante();
+        }
+    }
+}
